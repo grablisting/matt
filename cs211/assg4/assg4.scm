7014b83713651b922acfd83d1afc08535c34a537
Matt Forbes <matt@grablisting.com>
2010-05-24 16:54:43 -0700

    hello

--- cs211/assg4/assg4.scm
+++ cs211/assg4/assg4.scm
@@ -2,20 +2,22 @@
   (lambda (elem set)
     (cond ( (null? set) #f)
           ( (list? elem) (if (list? (car set)) 
-                             (set-equal? elem (car set)) 
+                             (and (set-equal? elem (car set)
+                                  (memb? elem (cdr set)))) 
                              #f))
           ( (eq? elem (car set)) #t)
           ( else (memb? elem (cdr set))))))
 
-(define xor
-  (lambda (t1 t2)
-    (if (or (and t1 t2) (and (not t1) (not t2)))
-        (#f)
-        (#t))))
-
 (define set-equal?
   (lambda (set1 set2)
-    (cond ( (or (null? set1) (null? set2)) #f)
-          ( (not (xor (list? set1) (list? set2))) #f)
+    (cond ((and (null? set1) (null? set2)) #t)
+          ((or (null? set1) (null? set2)) #f)
+          ((not (and (list? set1) (list? set2))) #f)
+          ((and (list? (car set1)) (list? (car set2)))
+           (and (set-equal? (car set1) (car set2))
+                 (set-equal? (cdr set1) (cdr set2))))
+          ((or (list? (car set1)) (list? (car set2))) #f)
+          ((and (eq? (car set1) (car set2))
+                 (set-equal? (cdr set1) (cdr set2))) #t)
+          (else #f))))
           
-
