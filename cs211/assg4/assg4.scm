38c1b1131d99fea05ed9ba855004ef173406560a
Matt Forbes <matt@grablisting.com>
2010-05-25 10:15:54 -0700

    finished 211

--- cs211/assg4/assg4.scm
+++ cs211/assg4/assg4.scm
@@ -1,27 +1,77 @@
+; Matt Forbes
+; CSCI 211
+; Assignment 4 - Set operations
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; remove
+; takes an element and a set and returns the set without that element
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define remove
+  (lambda (elem set)
+    (cond((null? set) set)
+         ((and (list? elem) (list? (car set)))
+          (if (set-equal? elem (car set))
+              (remove elem (cdr set))
+              (cons (car set) (remove elem (cdr set)))))
+         ((and (not (list? elem)) (not (list? (car set))))
+          (if (eq? elem (car set))
+              (remove elem (cdr set))
+              (cons (car set) (remove elem (cdr set)))))
+         (else (cons (car set) (remove elem (cdr set)))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; memb?
+; takes an element and a set and returns true if the element is in the set
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define memb?
   (lambda (elem set)
     (cond ((null? set) #f)
-          ((list? elem) 
-           (if (list? (car set)) 
-               (or (set-equal? elem (car set))
-                   (memb? elem (cdr set)))
-               #f))
-          ( (eq? elem (car set)) #t)
-          ( else (memb? elem (cdr set))))))
+          ((and (list? elem) (list? (car set)))
+           (or (set-equal? elem (car set)) (memb? elem (cdr set))))
+          ((and (not (list? elem)) (not (list? (car set))))
+           (or (eq? elem (car set)) (memb? elem (cdr set))))
+          (else (memb? elem (cdr set))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; set-equal?
+; takes two sets and returns true if they contain exactly the same elements
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define set-equal?
   (lambda (set1 set2)
-    (cond ((and (null? set1) (null? set2)) #t)
-          ((or (null? set1) (null? set2)) #f)
-          ((not (and (list? set1) (list? set2))) #f)
-          ((and (list? (car set1)) (list? (car set2)))
-           (and (set-equal? (car set1) (car set2))
-                 (set-equal? (cdr set1) (cdr set2))))
-          ((or (list? (car set1)) (list? (car set2))) #f)
-          ((and (eq? (car set1) (car set2))
-                 (set-equal? (cdr set1) (cdr set2))) #t)
-          (else #f))))
+    (cond((null? set1) (null? set2))
+         ((null? set2) (null? set1))
+         (else
+          (and (memb? (car set1) set2)
+               (memb? (car set2) set1)
+               (set-equal? (remove (car set2) (cdr set1)) (remove (car set1) (cdr set2))))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; subset?
+; takes two sets and returns true if all of the elements in the first
+; set are elements in the second set
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define subset?
+  (lambda (set1 set2)
+    (if (null? set1)
+        #t
+        (and (memb? (car set1) set2)
+             (subset? (cdr set1) set2)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; union
+; takes two sets and returns a new set with all of the elements that are
+; in either the first set, the second set, or both
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define union
   (lambda (set1 set2)
     (cond((null? set1) set2)
@@ -30,9 +80,16 @@
           (let ((elem (car set1))
                 (rest (cdr set1)))
             (if (memb? elem set2)
-                   (union rest set2)
-                   (cons elem (union rest set2))))))))
+                (union rest set2)
+                (cons elem (union rest set2))))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; intersection
+; takes two sets and returns a new set with only the elements that are
+; in both sets
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define intersection
   (lambda (set1 set2)
     (if (or (null? set1) (null? set2))
@@ -43,6 +100,13 @@
                (cons elem (intersection rest set2))
                (intersection rest set2))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; difference
+; takes two sets and returns a new set that contains all of the elements
+; that are in the first set but not the second
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define difference
   (lambda (set1 set2)
     (if (or (null? set1) (null? set2))
